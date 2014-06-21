f060fe4223978a05eda79e377c5a3dee88bdad1f
Michael James <michael@michaelbjames.com>
2014-06-21 20:14:25 +0200

    It's still a bit sensative and scrolling still propagates on FF30OSX

--- test.elm
+++ test.elm
@@ -1,5 +1,23 @@
 module Main where
 
 import Scroll
+import Mouse
 
-main = lift asText Scroll.delta
+sceneX : Int
+sceneX = 500
+sceneY : Int
+sceneY = 500
+
+updateScene : Float -> Form -> Form
+updateScene zoom scene =
+    let scaleFactor = e ^ zoom
+    in  scale scaleFactor scene
+
+
+emptyScene : Form
+emptyScene = circle 20 |> filled red
+                       |> move (0,0)
+
+
+--main = lift asText Scroll.delta
+main = lift (\x -> collage sceneX sceneY [x]) <| foldp updateScene emptyScene Scroll.delta
