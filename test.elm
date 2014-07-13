198dfb17be14c5cc78b1f084e01bad9d20ece170
Michael James <michael@michaelbjames.com>
2014-07-13 18:16:16 +0200

    this is so frustrating

--- test.elm
+++ test.elm
@@ -3,6 +3,7 @@ module Main where
 import Scroll
 import Mouse
 import Window
+import Debug
 
 type Position = { x : Float, y : Float }
 type Dimensions = { width : Float, height : Float }
@@ -20,30 +21,23 @@ type Update =
 
 -- The idea is to pass aroud the center of the system along with the form too.
 updateScene : Update -> Scene -> Scene
-updateScene update oldScene =
-    let zoomed = zoom update oldScene
-    in  pan update zoomed
+updateScene = panZoom 
 
 
-zoom : Update -> Scene -> Scene
-zoom update oldScene =
-    let scaleFactor = (1.1) ^ update.scrollDelta
-    in { oldScene | form <- scale scaleFactor oldScene.form}
-
-pan : Update -> Scene -> Scene
-pan update oldScene =
-    let zoomDirection = if update.scrollDelta >= 0 then -1 else 1
-        adjustedMouse = { x = (min update.mouse.x update.window.width) - update.window.width/2
+panZoom : Update -> Scene -> Scene
+panZoom update oldScene =
+    let scroll = (1.1) ^ update.scrollDelta
+        centeredMouse = { x = (min update.mouse.x update.window.width) - update.window.width/2
                         , y = update.window.height/2 - (min update.mouse.y update.window.height)
                         }
-        panTo mouse = { x = zoomDirection * 0.02 * (mouse.x - oldScene.center.x)
-                      , y = zoomDirection * 0.02 * (mouse.y - oldScene.center.y)
-                      }
-        newCenter = panTo adjustedMouse
-    in  { oldScene |
-          form <- move (newCenter.x, newCenter.y) oldScene.form
-        , center <- newCenter}
+        translationDiff pt1 pt2 = { dx = pt2.x - pt1.x
+                                  , dy = pt2.y - pt1.y }
+        translateOffset = Debug.log "offset" <| translationDiff oldScene.center centeredMouse
+        centeredScene = move (translateOffset.dx,translateOffset.dy) oldScene.form
+        zoomedScene = scale scroll centeredScene
+    in { oldScene | form <- zoomedScene, center <- centeredMouse}
 
+                
 
 emptyScene : Scene
 emptyScene =
